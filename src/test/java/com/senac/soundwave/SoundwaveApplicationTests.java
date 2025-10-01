0c22a5297edb9a2da4e9c1ed3dfbb04b948d8ce8
peeduardo <pedroesasantos130805@gmail.com>
2025-10-01 16:55:53 -0300

    Commit inicial com o arquivo Spring Boot na pasta original

---
+++ src/test/java/com/senac/soundwave/SoundwaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.soundwave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundwaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
